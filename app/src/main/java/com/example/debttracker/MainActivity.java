6c9121c355cba6c5f2765c8e11e2ff53af05a606
JennySimen <yakamjennifer@gmail.com>
2020-08-20 16:46:26 +0100

    Created the welcome screen

---
+++ app/src/main/java/com/example/debttracker/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.debttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    CarouselView carouselView;
+    int NUMBER_OF_PAGES = 3;
+
+    int[] sampleImages = {R.drawable.moneygirl, R.drawable.moneygirl2, R.drawable.moneygirl3};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+
+        carouselView.setImageListener(imageListener);
+    }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+}
